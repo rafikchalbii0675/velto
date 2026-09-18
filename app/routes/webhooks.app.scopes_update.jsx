1d2732472051024d3c971b190c5f866b0decc88c
Rafik <ton@email.com>
2026-09-18 18:01:16 -0400

    fix: downgrade shopify remix to 3.7.0

--- app/routes/webhooks.app.scopes_update.jsx
+++ app/routes/webhooks.app.scopes_update.jsx
@@ -1,5 +1,5 @@
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+import { prisma as db } from "../db.server";
 
 export const action = async ({ request }) => {
   const { payload, session, topic, shop } = await authenticate.webhook(request);
